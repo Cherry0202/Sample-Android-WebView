4cf479444eb529e4db19dddb394f156471a0f4e7
Cherry <ex00ia2432@gmail.com>
2020-10-16 15:08:11 +0900

    done

---
+++ app/src/main/java/jp/ac/hal/webhogehoge/MainActivity.java
@@ -0,0 +1,29 @@
+package jp.ac.hal.webhogehoge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView = null;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // WebView呼び出し
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+//        webview内でのjsを許
+        myWebView.getSettings().setJavaScriptEnabled(true);
+//       webAppInterfaceを使う
+        myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        myWebView.loadUrl("file:///android_asset/index.html"); // ローカルのhtmlファイルを指定
+    }
+}

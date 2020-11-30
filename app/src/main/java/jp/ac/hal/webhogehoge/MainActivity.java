3390b3eba14ac519e1979e4022f1ce969eb4852f
Cherry <ex00ia2432@gmail.com>
2020-11-30 13:33:45 +0900

    [clean]

--- app/src/main/java/jp/ac/hal/webhogehoge/MainActivity.java
+++ app/src/main/java/jp/ac/hal/webhogehoge/MainActivity.java
@@ -35,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
 		myWebView.getSettings().setJavaScriptEnabled(true);
 //       webAppInterfaceを使う
 		myWebView.addJavascriptInterface(new ThreadManager(this.bluetoothAdapter), "Android");
-		myWebView.loadUrl("file:///android_asset/index2.html");
-//		myWebView.loadUrl("file:///android_asset/dist/index.html");
+//		myWebView.loadUrl("file:///android_asset/index2.html");
+		myWebView.loadUrl("file:///android_asset/dist/index.html");
 	}
 
 	//　初回表示、ポーズからの復帰時
